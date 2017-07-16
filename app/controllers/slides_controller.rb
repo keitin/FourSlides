2550d06a3d2e57c3ff3648503852ad5037b31aa6
松下慶大 <atiek1121@gmail.com>
2017-07-16 13:06:12 +0900

    add slide new view

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,4 +1,18 @@
 class SlidesController < ApplicationController
   def index
   end
+
+  def new
+    @slide = Slide.new
+  end
+
+  def create
+    Slide.create(slide_params)
+    redirect_to :root
+  end
+
+  private
+  def slide_params
+    params.require(:slide).permit(:title, :summary)
+  end
 end
